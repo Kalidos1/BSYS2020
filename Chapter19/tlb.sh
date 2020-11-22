24fed862168bd259f9adb09ca334c54a2eca5c99
Andre Göhring <an851goe@ct-bsys-ws20-18.syslab.in.htwg-konstanz.de>
2020-11-22 14:12:22 +0100

    test

--- Chapter19/tlb.sh
+++ Chapter19/tlb.sh
@@ -4,9 +4,12 @@ numPages="$1"
 trials="$2"
 
 
-for counter in {1..50}
+for counter in {1..10}
 do
-    "$(dirname)"./tlb "$numPages" "$trials"
+    echo "NEUE ITERATION"
+    echo "$numPages"
+    echo "$trials"
+    ./tlb "$numPages" "$trials"
     numPages=$((numPages*2))
     trials=$((numPages*2))
 done
