69ebbc88264cfff8a8b4a6e9504346e98a30106d
Andre Göhring <a-goehring@web.de>
2020-11-22 15:06:00 +0100

    New Chapters

--- Chapter19/tlb.c
+++ Chapter19/tlb.c
@@ -21,80 +21,66 @@ int main(int argc, char const *argv[])
         unsigned long tlbAccessTime, loopTime;
         struct timespec startTLBAccess, stopTLBAccess;
         int jump;
-        int* array = (int*) malloc((counter * NUMPAGES) * sizeof(int));
-        const int PAGESIZE = 4096; // Im Container mit -> "getconf PAGESIZE"
 
+        const int PAGESIZE = 4096; // Im Container mit -> "getconf PAGESIZE"
+        jump = PAGESIZE / sizeof(int);
 
         printf("Number of Pages %i\n", NUMPAGES);
         printf("Number of Iterations %i\n", counter);
 
-        //-----------TLB Call--------------
-        if (clock_gettime(CLOCK_MONOTONIC_RAW, &startTLBAccess) < 0) { //CLOCK_REALTIME/CLOCK_MONOTONIC gehen auch
-            printf("Start-Clock failed\n");                                        // -> Alle ca. 500ns
-            free(array);
-            exit(1);
-        }
 
-        jump = PAGESIZE / sizeof(int);
-        for (int i = 0; i <= counter; i++) {
-            for (int j = 0; j < NUMPAGES * jump; j += jump) {
-                array[j] += 1;
+        for (size_t pageCounter = 1; pageCounter < NUMPAGES + 1; pageCounter++) {
+
+            int* array = (int*) malloc((counter * NUMPAGES) * sizeof(int));
+
+            if (array == NULL) {
+                fprintf(stderr, "Error allocating memory: ");
+                exit(EXIT_FAILURE);
             }
-        }
 
-        if (clock_gettime(CLOCK_MONOTONIC_RAW, &stopTLBAccess) < 0) {
-            printf("Stop-Clock failed\n");
+            long long totalTlbAccessTime = 0;
+
+            for (int i = 0; i < counter; i++) {
+
+                for (int j = 0; j < NUMPAGES * jump; j += jump) {
+
+                    //-----------TLB Call--------------
+                    if (clock_gettime(CLOCK_MONOTONIC_RAW, &startTLBAccess) < 0) { //CLOCK_REALTIME/CLOCK_MONOTONIC gehen auch
+                        printf("Start-Clock failed\n");                                    // -> Alle ca. 500ns
+                        free(array);
+                        exit(1);
+                    }
+
+                    array[j] += 1;
+
+                    if (clock_gettime(CLOCK_MONOTONIC_RAW, &stopTLBAccess) < 0) {
+                        printf("Stop-Clock failed\n");
+                        free(array);
+                        exit(1);
+                    }
+
+                    //-----------TLB Call Time--------------
+                    // -> Umrechnung, falls erster Zeitpunkt < zweiter Zeitpunkt
+                    if (startTLBAccess.tv_nsec > stopTLBAccess.tv_nsec) {
+                        tlbAccessTime += (((stopTLBAccess.tv_sec - 1) - startTLBAccess.tv_sec) * billion)
+                                        + ((stopTLBAccess.tv_nsec + billion) - startTLBAccess.tv_nsec);
+                    } else {
+                        // Berechnung der Zeit
+                        tlbAccessTime += (stopTLBAccess.tv_sec - startTLBAccess.tv_sec) +
+                                        (stopTLBAccess.tv_nsec - startTLBAccess.tv_nsec);
+                    }
+                }
+                totalTlbAccessTime += tlbAccessTime / pageCounter;
+            }
+
+            printf("tlbAccessTime: %ld\n", tlbAccessTime);
+            unsigned long calcTime = (totalTlbAccessTime / counter);
+            printf("\nOne TLB-Access takes %ld ns\n", calcTime);
+
             free(array);
-            exit(1);
+
         }
 
-        //-----------LOOP TIME--------------
-
-        struct timespec startLoop, stopLoop;
-
-        if (clock_gettime(CLOCK_MONOTONIC_RAW, &startLoop) < 0) {
-            printf("Start-Clock failed\n");
-            free(array);
-            exit(1);
-        }
-
-        for (int i = 0; i <= counter; i++) {
-        }
-
-        if (clock_gettime(CLOCK_MONOTONIC_RAW, &stopLoop) < 0) {
-            printf("Stop-Clock failed\n");
-            free(array);
-            exit(1);
-        }
-
-        //-----------SYSTEM CALL TIME--------------
-        // -> Umrechnung, falls erster Zeitpunkt < zweiter Zeitpunkt
-        if (startTLBAccess.tv_nsec > stopTLBAccess.tv_nsec) {
-            tlbAccessTime = (((stopTLBAccess.tv_sec - 1) - startTLBAccess.tv_sec) * billion)
-                            + ((stopTLBAccess.tv_nsec + billion) - startTLBAccess.tv_nsec);
-        } else {
-            // Berechnung der Zeit
-            tlbAccessTime = (stopTLBAccess.tv_sec - startTLBAccess.tv_sec) +
-                            (stopTLBAccess.tv_nsec - startTLBAccess.tv_nsec);
-        }
-
-        //-----------LOOP TIME--------------
-        // -> Umrechnung, falls erster Zeitpunkt < zweiter Zeitpunkt
-        if (startLoop.tv_nsec > stopLoop.tv_nsec) {
-            loopTime = (((stopLoop.tv_sec - 1) - startLoop.tv_sec) * billion)
-                       + ((stopLoop.tv_nsec + billion) - startLoop.tv_nsec);
-        } else {
-            // Berechnung der Zeit
-            loopTime = (stopLoop.tv_sec - startLoop.tv_sec) + (stopLoop.tv_nsec - startLoop.tv_nsec);
-        }
-
-        printf("tlbAccessTime: %ld\n", tlbAccessTime);
-        printf("loopTime: %ld\n", loopTime);
-        unsigned long calcTime = (tlbAccessTime / counter) - (loopTime / counter);
-        printf("\nOne TLB-Access takes %ld ns\n", calcTime);
-
-
-        free(array);
         return 0;
     }
 }
