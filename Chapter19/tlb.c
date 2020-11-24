8df759a5ec787c06808e098562303386b9ed6d7a
Andre Göhring <a-goehring@web.de>
2020-11-24 15:11:45 +0100

    Changes

--- Chapter19/tlb.c
+++ Chapter19/tlb.c
@@ -1,3 +1,6 @@
+// Gruppe 18: Andre Göhring , Kira Koch
+// Code Chapter 19 - tlb.c
+
 #define _GNU_SOURCE
 #include <sched.h>
 #include <stdlib.h>
@@ -32,7 +35,7 @@ int main(int argc, char const *argv[])
 
         cpu_set_t set;
         CPU_ZERO(&set);
-        CPU_SET(4, &set);
+        CPU_SET(4, &set); // Speziell für unseren Container -> //CPU 4,7-8,11 sind Möglich -> "cat /sys/fs/cgroup/cpuset/cpuset.cpus"
         if (sched_setaffinity(0, sizeof(set), &set)) {
             perror("error setting sched_affinity");
             _exit(EXIT_FAILURE);
@@ -43,20 +46,20 @@ int main(int argc, char const *argv[])
 
             long long totalTlbAccessTime = 0;
 
-            for (volatile int i = 1; i < counter; i++) {
+            for (int i = 1; i < counter; i++) {
 
-                int* array = (int*) calloc((counter * NUMPAGES), sizeof(int));
+                int* array = (int*) calloc((counter * NUMPAGES), sizeof(int)); //malloc
 
                 if (array == NULL) {
                     fprintf(stderr, "Error allocating memory: ");
                     exit(EXIT_FAILURE);
                 }
 
-                for (volatile int j = 1; j < NUMPAGES * jump; j += jump) {
+                for (int j = 1; j < NUMPAGES * jump; j += jump) {
 
                     //-----------TLB Call--------------
                     if (clock_gettime(CLOCK_MONOTONIC_RAW, &startTLBAccess) < 0) { //CLOCK_REALTIME/CLOCK_MONOTONIC gehen auch
-                        printf("Start-Clock failed\n");                                    // -> Alle ca. 500ns
+                        printf("Start-Clock failed\n");
                         free(array);
                         exit(1);
                     }
@@ -79,9 +82,8 @@ int main(int argc, char const *argv[])
                         tlbAccessTime += (stopTLBAccess.tv_sec - startTLBAccess.tv_sec) +
                                         (stopTLBAccess.tv_nsec - startTLBAccess.tv_nsec);
                     }
-
+                    totalTlbAccessTime += tlbAccessTime / j;
                 }
-                totalTlbAccessTime += tlbAccessTime / i;
                 free(array);
             }
 
