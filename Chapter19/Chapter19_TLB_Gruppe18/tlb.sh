76fe0e22d43dd8334105bbc8b6eb88f6a5ec1333
Andre Göhring <a-goehring@web.de>
2020-11-24 15:40:04 +0100

    Changes

---
+++ Chapter19/Chapter19_TLB_Gruppe18/tlb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+numPages="$1"
+trials="$2"
+
+
+for counter in {1..10}
+do
+    echo "NEUE ITERATION"
+    echo "$numPages"
+    echo "$trials"
+    ./tlb "$numPages" "$trials"
+    numPages=$((numPages*2))
+    trials=$((trials*2))
+done
