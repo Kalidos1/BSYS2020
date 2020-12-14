5c560e7d985d0604dc646faafb254aae01ade29f
Andre Göhring <an851goe@ct-bsys-ws20-18.syslab.in.htwg-konstanz.de>
2020-12-14 13:03:20 +0100

    changes

--- Chapter27/HW-Threads-RealAPI/main-race.c
+++ Chapter27/HW-Threads-RealAPI/main-race.c
@@ -3,16 +3,22 @@
 #include "mythreads.h"
 
 int balance = 0;
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m2 = PTHREAD_MUTEX_INITIALIZER;
 
 void* worker(void* arg) {
-    balance++; // unprotected access 
+    Pthread_mutex_lock(&m1);
+    balance++; // unprotected access
+    Pthread_mutex_unlock(&m1); 
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
-    balance++; // unprotected access
+    Pthread_mutex_lock(&m1);
+    balance++;// unprotected access
+    Pthread_mutex_unlock(&m1);
     Pthread_join(p, NULL);
     return 0;
 }
