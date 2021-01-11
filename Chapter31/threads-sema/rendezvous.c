accb9f0ce292a05d7442011458a88e857255c27f
Andre Göhring <a-goehring@web.de>
2021-01-11 15:38:20 +0100

    Changes

--- Chapter31/threads-sema/rendezvous.c
+++ Chapter31/threads-sema/rendezvous.c
@@ -12,6 +12,7 @@ void *child_1(void *arg) {
     printf("child 1: before\n");
     sem_post(&s1);
     sem_wait(&s2);
+    sleep(1);
     printf("child 1: after\n");
     return NULL;
 }
@@ -20,6 +21,7 @@ void *child_2(void *arg) {
     printf("child 2: before\n");
     sem_post(&s2);
     sem_wait(&s1);
+    sleep(1);
     printf("child 2: after\n");
     return NULL;
 }
