275e6cdf9c78b5f34e330ada46bafd202d639624
Andre Göhring <a-goehring@web.de>
2021-01-11 16:23:54 +0100

    Changes

--- Chapter31/threads-sema/mutex-nostarve.c
+++ Chapter31/threads-sema/mutex-nostarve.c
@@ -10,25 +10,56 @@
 // when attempting to acquire this mutex you build?
 //
 
-typedef __ns_mutex_t {
+typedef struct __ns_mutex_t {
+    int counter;
+    sem_t s1, s2;
 } ns_mutex_t;
 
 void ns_mutex_init(ns_mutex_t *m) {
+    sem_init(&m->s1, 0 , 1);
+    sem_init(&m->s2, 0 , 1);
+    m->counter = 0;
 }
 
 void ns_mutex_acquire(ns_mutex_t *m) {
+    int tid = syscall(SYS_gettid);
+    if ((m->counter % tid) == 0) {
+        sem_wait(&m->s1);
+    } else {
+        sem_wait(&m->s2);
+        sem_wait(&m->s1);
+    }
+    sem_wait(&m->s1);
+    printf("%d\n",tid);
 }
 
 void ns_mutex_release(ns_mutex_t *m) {
+    m->counter++;
+    sem_post(&m->s1);
+    sem_post(&m->s2);
+    sleep(1);
 }
 
 
 void *worker(void *arg) {
+    for (int i = 0; i < 100; i++) {
+        ns_mutex_acquire(arg);
+        printf("Test\n");
+        ns_mutex_release(arg);
+    }
     return NULL;
 }
 
+ns_mutex_t mutex;
+
 int main(int argc, char *argv[]) {
+    pthread_t p1, p2;
     printf("parent: begin\n");
+    ns_mutex_init(&mutex);
+    Pthread_create(&p1, NULL, worker, &mutex);
+    Pthread_create(&p2, NULL, worker, &mutex);
+    Pthread_join(p1, NULL);
+    Pthread_join(p2, NULL);
     printf("parent: end\n");
     return 0;
 }
